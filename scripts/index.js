11070b54f07040edeb8623f82060e4eec3745ed3
Mirakul-dev <miracleonyenma1@gmail.com>
2024-12-28 15:17:49 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,190 @@
+const html = `
+    <header class="Header">
+      <div class="left-section">
+        <div>
+          <P>Home</P>
+        </div>
+        <div >
+          <P>Resume</P>
+        </div>
+        <div>
+          <P>Projects</P>
+        </div>
+        <div>
+          <P>Contact</P>
+        </div>
+      </div>
+    </header>
+
+
+    <main>
+
+      <div class="page">
+
+        <div class="heading">
+          <div>
+            <div>
+              <p> Hi, my name is </p>
+              <p class="first-text"> Miracle Onyenma <br> Frontend Developer <br> &amp UI/UX Designer
+              </p>
+              <p>Versatile Frontend Dev passionate about exceptional digital <br>experiences specializing in design,user experience</p>
+            </div>
+    
+            <div class="button">
+              <button class="resume-button"> RESUME </button>
+              <button class="Projects-button"> PROJECTS </button>
+            </div>
+          </div>
+          <div>
+            <img class="header-image"  src="project-pictures/boy-studying-peacefully-library_23-2148844707.avif">
+          </div>
+        </div>
+    
+    
+    
+        <hr>
+    
+        <div class="projects">
+    
+          <p class="project-text"> My Projects </p>
+          <div class="projects-grid">
+            <div class="first-project">
+              <div class="thumbnail-row">
+                <img class="thumbnail" src="project-pictures/Screenshot 2024-11-18 145257.png">
+              </div>
+              <div class="project-info-grid">
+                <p> Amazon Clone</p>
+                <p> An E-commerce store for buying and selling of goods</p>
+                <p class="view">view project</p>
+              </div>
+            </div>
+    
+            <div class="second-project">
+              <div class="thumbnail-row">
+                <img class="thumbnail" src="project-pictures/black-office-business-equipment-black-background_1387-559.avif">
+              </div>
+              <div class="project-info-grid">
+                <p> Calcultor web App</p>
+                <p> A web application calculator</p>
+                <p class="view">view project</p>
+              </div>
+            </div>
+    
+            <div class="third-project">
+              <div class="thumbnail-row">
+                <img class="thumbnail" src="project-pictures/close-up-view-online-shopping-concept_23-2148625687.avif">
+              </div>
+              <div class="project-info-grid">
+                <p> Amazon Clone</p>
+                <p> An E-commerce store for buying and selling of goods</p>
+                <p class="view">view project</p>
+              </div>
+            </div>
+    
+            
+      
+          <button class="see-button"> SEE MORE </button>
+          
+          
+    
+        </div>
+    
+        <hr>
+    
+        <div class="about-me">
+          <p class="about-text"> About Me </p>
+          <div class="about">
+            <div>
+    
+              
+    
+              <p class="hello">
+                Hello there! My name is <br> Miracle, and I'm delighted <br> to connect with you.
+              </p>
+    
+              <P>
+                As a versatile Frontend Developer, I'm deeply passionate <br> about crafting remarkable digital experiences. Beyond coding,<br> I excel in UI/UX Design . My focus lies in designing and <br>
+                implementing captivating visual elements for websites and <br> applications. I find great joy in sharing my expertise and <br> knowledge with others, fostering learning and growth within <br> our community. Let's build something incredible together!
+              </P>
+    
+              
+            </div>
+          
+            <div>
+              <img class="header-image"  src="project-pictures/businessman-colleague-shaking-hands_107420-84915.avif">
+            </div>
+          </div>
+        </div>
+       
+        <hr>
+    
+        <div class="contact">
+          <div class="contact-details">
+            <p class="contact-text">Contact</p>
+            <input class="name-text" type="text" placeholder="Name">
+    
+            <input class="email-text" type="text" placeholder="Email">
+    
+            <input class="message-text" type="text" placeholder="Message">
+    
+            <button class="submit-button">SUBMIT</button>
+          </div>
+    
+          <div class="talk">
+            <div>
+              <P class="Let">Let's Talk</P>
+              <p>Have some big idea or brand to develop and need <br>help? Then reach out I'd love to hear about your <br> project and provide help</p>
+            </div>
+    
+            <div>
+              <p class="email">Email</p>
+              <p>miracleonyenma1@gmail.com</p>
+            </div>
+    
+            <div>
+              <p class="socials">Socials</p>
+              <p>instagram</p>
+              <p>Youtube</p>
+              <p>twitter</p>
+            </div>
+          </div>
+    
+        </div>
+  
+  
+      </div>
+     
+
+    </main>
+    `;
+    
+    const form =document.getElementById ('form')
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault()
+      alert ("Thank you for contacting us, we will give you a feedback shortly")
+
+      const name = document.getElementById('name').value
+
+      console.log(name)
+
+      const email = document.getElementById('email').value
+
+      console.log (email)
+
+      const message = document.getElementById('Message').value
+
+      console.log (message)
+    })
+
+    function showSidebar(){
+      const sidebar = document.querySelector('.sidebar')
+       sidebar.style.display = 'flex'
+         
+     }
+ 
+    function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'none'
+        
+    }
